9e08bc12bd69d3195bf9703675f3d066a17c0631
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-09-23 19:47:53 -0300

    novos exercicios de function

---
+++ mod4/1.js
@@ -0,0 +1,37 @@
+//functions 
+function nomeUser(){
+    console.log("My name is Jonas");
+}
+function soma(a,b){
+    return a+b
+}
+
+nomeUser();
+console.log(soma(4,5));
+//-------------------------
+//function declaration
+function calcAge(niver){
+    return 2037-niver;
+}
+console.log(calcAge(1998));
+
+//function expression 
+let calAge2 = function (age) {
+    return 2037-age;
+} 
+let age2 = calAge2(1990);
+console.log(age2);
+//arrow functions
+const aniver = ano => 2037-ano;
+console.log("Arrow function exemplo 001 >> ",aniver(1991));
+
+const anosAposentar = anoNiver => {
+    const age = 2037-anoNiver;
+    const aposentar = 65-age;
+    return aposentar;
+}
+console.log("Exemplo Arrow Functions 002 >> ",anosAposentar(1992));
+const subtracao = (numeroUm,numerodois)=>{
+    return numeroUm-numerodois;
+}
+console.log("Arrow function 003 >> ",subtracao(200,102));
