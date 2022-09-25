4bac6b02a5a11e581eda87a20c6a689f4bc3337d
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-09-25 18:48:17 -0300

    novos exercicios

--- loops/1.js
+++ loops/1.js
@@ -1,8 +1,27 @@
 //loops 
 const loopsFor = "Loop FOR"
-for(let repetir=0;repetir <=10;repetir++){
-    console.log(loopsFor + ' ' +repetir);
+for (let repetir = 0; repetir <= 10; repetir++) {
+    console.log(loopsFor + ' ' + repetir);
 }
 
+//loops with arrays, objects 
 
+const pessoa = [
+    'jonas',
+    'Gomez',
+    2037 - 1998,
+    'medico',
+    ['michale', 'joao', 'carlos']
+];
+
+const types = [];
+
+for (let i = 0; i <= pessoa.length; i++) {
+    console.log(pessoa[i], typeof pessoa[i]);
+
+    //types[i] = typeof pessoa[i];
+    types.push(typeof pessoa[i]);
+
+}
+console.log(types);
 
