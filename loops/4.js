d32447492f926a2782ec99f76bfccfe5dcb53eb7
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-09-26 19:46:54 -0300

    fim 1 modulo

--- loops/4.js
+++ loops/4.js
@@ -1,14 +1,14 @@
 //teste 
 
-let bills = [22,295,176,440,37,105,10,1100,86,52];
+let bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 let tips = [];
 let totals = [];
 
 const calcTip = (bill) => {
-    return bill >= 50 && bill <= 300? bill * 0.15:bill *0.2;
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
 
-for(let x=0;x<bills.length;x++){
+for (let x = 0; x < bills.length; x++) {
     const tip = calcTip(bills[x]);
     tips.push(tip);
     totals.push(tip + bills[x]);
@@ -17,4 +17,16 @@ console.log(tips);
 console.log(totals);
 console.log(bills);
 
+//bonus 
+
+const calTwo = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+console.log(calTwo([2,3,6]));
+console.log(calTwo(totals));
+console.log(calTwo(tips));
 
