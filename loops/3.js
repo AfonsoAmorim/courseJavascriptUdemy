4a4d909b74be35efe42e1c2213227cea272b6904
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-09-26 19:40:20 -0300

    teste loops

---
+++ loops/3.js
@@ -0,0 +1,9 @@
+// loop while 
+
+let rep = 1;
+while (rep <= 20) {
+    console.log("Loops While na vez: ", rep);
+    rep++;
+};
+
+
---
+++ loops/4.js
@@ -0,0 +1,20 @@
+//teste 
+
+let bills = [22,295,176,440,37,105,10,1100,86,52];
+let tips = [];
+let totals = [];
+
+const calcTip = (bill) => {
+    return bill >= 50 && bill <= 300? bill * 0.15:bill *0.2;
+}
+
+for(let x=0;x<bills.length;x++){
+    const tip = calcTip(bills[x]);
+    tips.push(tip);
+    totals.push(tip + bills[x]);
+}
+console.log(tips);
+console.log(totals);
+console.log(bills);
+
+
