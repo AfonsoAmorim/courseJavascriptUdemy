393a43e7c9aa7952bdceec0887c4827b2cf000d5
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-09-26 19:20:30 -0300

    novos exercicios loops

--- loops/1.js
+++ loops/1.js
@@ -24,4 +24,13 @@ for (let i = 0; i <= pessoa.length; i++) {
 
 }
 console.log(types);
+//teste two
+
+const years = [1997,1990,2000,1960];
+const ages = [];
+for(i=0;i < years.length;i++){
+    ages.push(2037-years[i]);
+}
+console.log("Incluindo dados em um novo array ",ages);
+
 
---
+++ loops/2.js
@@ -0,0 +1,13 @@
+const pessoa = [
+    'jonas',
+    'Gomez',
+    2037 - 1998,
+    'medico',
+    ['michale', 'joao', 'carlos']
+];
+
+// inverter impressão 
+for (let i = pessoa.length - 1; i >= 0; i--) {
+    console.log(i, pessoa[i]);
+}
+
