59629101dc3e488f5174237a11a19f48b6d6678e
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-09-30 23:07:14 -0300

    novos exercicios

--- project001/script.js
+++ project001/script.js
@@ -1,8 +1,22 @@
 'use strict';
-document.querySelector('.message').textContent = "Correct Number,now!!";
+// document.querySelector('.message').textContent = "Correct Number,now!!";
 
-document.querySelector('.number').textContent = 15;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 15;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 200;
+// document.querySelector('.guess').value = 200;
+
+const x = function(){
+    console.log(23);
+}
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = document.querySelector('.guess').value;
+
+    if(!guess){
+        document.querySelector('.message').textContent = 'No number!';
+    }
+});
+
+
 
