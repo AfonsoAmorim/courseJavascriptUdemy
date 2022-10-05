c3c22ceb6c28c802409e1634f89cb87d22701cfe
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-10-04 22:39:02 -0300

    novas funções

--- project001/script.js
+++ project001/script.js
@@ -10,13 +10,19 @@ const x = function(){
     console.log(23);
 }
 
+const number = Math.trunc(Math.random() * 20) +1;
+document.querySelector('.number').textContent = number;
+
+
+
 document.querySelector('.check').addEventListener('click', function(){
     const guess = document.querySelector('.guess').value;
-
+    console.log(guess, typeof guess);
     if(!guess){
         document.querySelector('.message').textContent = 'No number!';
     }
 });
+
 
 
 
